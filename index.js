c6098ce55967f7d97dc7c7ef20c8887f4d826851
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-01-21 22:56:31 -0500

    Finalzed code for application

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a short description for your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How do you install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide information on your projects usage:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please provide guidelines on contributing for this project:',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Please provide testing instructions for this project:',
+        name: 'testing',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for this project:',
+        choices: ['MIT', 'Apache', 'ISC', 'BSD'],
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            const readmeContent = generateMarkdown(answers);
+            fs.writeFile('myREADME.md', readmeContent, err => {
+                if (err) {
+                  console.error(err);
+                } else {
+                  console.log('README file created successfully!');
+                }
+              });
+            })
+            .catch(error => {
+              console.error(error);
+            });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
+  ![badge](https://img.shields.io/badge/license-${answers.license}-red)
+
+  ## Description
+  ${answers.description}
+
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+
+  ## Installation
+  ${answers.installation}
+
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  This project is licensed under the ${answers.license} license. Please visit https://choosealicense.com/licenses/${answers.license.toLowerCase()}/ for more information.
+
+  ## Contributing
+  ${answers.contributing}
+
+  ## Tests
+  ${answers.testing}
+
+  ## Questions
+  GitHub: https://github.com/${answers.username}/
+  For all questions please reach out via email to ${answers.email} and I will get back to you.
 `;
 }
 
